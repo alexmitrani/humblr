fb0b8e23b7a627f8c78f8e515c4d28a3386f2a1a
Alex Mitrani <27974470+alexmitrani@users.noreply.github.com>
2021-10-28 23:31:43 -0300

    Added @param mygraphtextsize the size of all text elements on the graph

--- R/mcmodelr.R
+++ R/mcmodelr.R
@@ -4,7 +4,7 @@
 #' @name mcmodelr
 #' @title monte carlo simulation function for use with mcr and model_ols_gpm
 #' @description takes model_ols_gpm, passes it input data produced with mcr, and passes back key results
-#' 
+#'
 #'
 #' @param myinputspreadsheet is the name of the spreadsheet with the input data.
 #' @param myinputdatasheet the name of the sheet with the input data.
@@ -19,6 +19,7 @@
 #' @param myseed1 the first random number seed
 #' @param myseed2 the second random number seed
 #' @param mygraphsize the pixel dimension for the output graphs
+#' @param mygraphtextsize the size of all text elements on the graph.
 #' @param mytag a character string that will be used in the output file names.  #'
 #' @param nsims the desired number of Monte Carlo simulations
 #'
@@ -26,49 +27,50 @@
 #' @export
 #'
 #' @examples
-#' 
+#'
 #' nsims <- 1000
-#' 
+#'
 #' myinputfile <- system.file("extdata", "risk_variable_distributions.xlsx", package = "humblr")
 #' mytest1 <- mcr(myinputfile, nsims = nsims, myseed = 12345L)
 #' mytest1
-#' 
+#'
 #' mycorrmatfile <- system.file("extdata", "correlation_matrix.xlsx", package = "humblr")
 #' mytest2 <- mcr(myinputfile, mycorrmatfilename = mycorrmatfile, nsims = nsims, myseed = 12345L)
 #' mytest2
-#' 
+#'
 #' myinputfile <- system.file("extdata", "model_ols_gpm_test.xlsx", package = "humblr")
-#' mymodel_results <- mcmodelr(nsims = nsims, myinputspreadsheet = myinputfile, myinputdatasheet = "data", mypredictiondf = mytest2, myresponse_ols = "depvar", myresponse_gpm = "residuals", myterms = c("var2",	"var3",	"var4",	"var5",	"var6",	"var7",	"var8",	"var9",	"var10"), sigmafrom = 1, sigmato= 10, sigmaby = 1, myfolds = 10, myseed1 = 123, myseed2 = 456, mygraphsize = 1000, mytag = "mymodel")
-#' 
-#' 
-#' 
-mcmodelr <- function(nsims = NULL, myinputspreadsheet = NULL, myinputdatasheet = NULL, mypredictiondf = NULL, myresponse_ols = NULL, myresponse_gpm = NULL, myterms = NULL, sigmafrom = NULL, sigmato= NULL, sigmaby = NULL, myfolds = NULL, myseed1 = NULL, myseed2 = NULL, mygraphsize = NULL, mytag = NULL){
-  
+#' mymodel_results <- mcmodelr(nsims = nsims, myinputspreadsheet = myinputfile, myinputdatasheet = "data", mypredictiondf = mytest2, myresponse_ols = "depvar", myresponse_gpm = "residuals", myterms = c("var2",	"var3",	"var4",	"var5",	"var6",	"var7",	"var8",	"var9",	"var10"), sigmafrom = 1, sigmato= 10, sigmaby = 1, myfolds = 10, myseed1 = 123, myseed2 = 456, mygraphsize = 1000, mygraphtextsize = 20, mytag = "mymodel")
+#'
+#'
+#'
+mcmodelr <- function(nsims = NULL, myinputspreadsheet = NULL, myinputdatasheet = NULL, mypredictiondf = NULL, myresponse_ols = NULL, myresponse_gpm = NULL, myterms = NULL, sigmafrom = NULL, sigmato= NULL, sigmaby = NULL, myfolds = NULL, myseed1 = NULL, myseed2 = NULL, mygraphsize = 1000, mygraphtextsize = 10, mytag = NULL){
+
   mymodel <- model_ols_gpm(myinputspreadsheet = myinputspreadsheet, myinputdatasheet = myinputdatasheet, mypredictiondf = mypredictiondf, myresponse_ols = myresponse_ols, myresponse_gpm = myresponse_gpm, myterms = myterms, sigmafrom = sigmafrom, sigmato= sigmato, sigmaby = sigmaby, myfolds = myfolds, myseed1 = myseed1, myseed2 = myseed2, mygraphsize = mygraphsize, mytag = mytag)
   myolsmodel <- mymodel[[1]]
   mygpmmodel <- mymodel[[2]]
   mypredictions <- mymodel[[3]]
-  
+
   myprediction <- mypredictions %>%
     select(prediction) %>%
     arrange(prediction) %>%
     mutate(prediction = exp(prediction)) %>%
     mutate(mycounter=1/nsims) %>%
     mutate(cumulative_proportion=cumsum(mycounter))
-  
+
   myplotfilename <- paste0(mytag, ".png")
-  
+
   png(file = myplotfilename, width = mygraphsize, height = mygraphsize)
-  
+
   print({
     p <- ggplot(myprediction, aes(x = prediction, y = cumulative_proportion))
     p + geom_line(size = 1)
+    p + theme(text = element_text(size = mygraphtextsize))
   })
-  
+
   dev.off()
-  
+
   prediction = as.vector(myprediction$prediction)
-  
+
   prediction_p0 = min(prediction)
   prediction_p1 = as.numeric(quantile(prediction,probs = 0.01))
   prediction_p10 = as.numeric(quantile(prediction,probs = 0.10))
@@ -78,11 +80,11 @@ mcmodelr <- function(nsims = NULL, myinputspreadsheet = NULL, myinputdatasheet =
   prediction_p90 = as.numeric(quantile(prediction,probs = 0.90))
   prediction_p99 = as.numeric(quantile(prediction,probs = 0.99))
   prediction_p100 = max(prediction)
-  
+
   myprediction_summary <- rbind(prediction_p0, prediction_p1, prediction_p10, prediction_p25, prediction_p50, prediction_p75, prediction_p90, prediction_p99, prediction_p100)
-  
+
   myreturnlist <- list(myolsmodel, mygpmmodel, mypredictions, myprediction_summary)
-  
+
   return(myreturnlist)
-  
+
 }
